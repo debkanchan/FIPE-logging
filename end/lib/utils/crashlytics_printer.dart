1561d04e4baea17eeb7c7ff2978bb968bb66aadb
Debkanchan Samadder <debu.samadder@gmail.com>
2024-12-27 22:12:19 +0530

    final touch

--- end/lib/utils/crashlytics_printer.dart
+++ end/lib/utils/crashlytics_printer.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:loggy/loggy.dart';
 import 'custom_loggy.dart';
@@ -24,7 +26,14 @@ class CrashlyticsPrinter extends PrettyPrinter {
       );
     }
 
-    FirebaseCrashlytics.instance.log(record.toString());
+    FirebaseCrashlytics.instance.log(json.encode({
+      "level": record.level.name,
+      "message": record.message,
+      "timestamp": record.time.millisecondsSinceEpoch,
+      "caller": record.loggerName,
+      "error": record.error,
+      "stackTrace": record.stackTrace,
+    }));
 
     super.onLog(record);
   }
